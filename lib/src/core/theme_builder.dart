5fcdcf18f6bd5d4dcf979e9cb7ca6ddd5bc84716
selawik <121557430+selawik@users.noreply.github.com>
2022-12-30 00:02:15 +0300

    Theme refactoring

--- lib/src/core/theme_builder.dart
+++ lib/src/core/theme_builder.dart
@@ -4,42 +4,52 @@ import 'app_colors.dart';
 
 class ThemeBuilder {
   static ThemeData get theme => ThemeData(
-        progressIndicatorTheme: const ProgressIndicatorThemeData(
-          color: AppColors.purple,
-        ),
+        progressIndicatorTheme: progressIndicatorTheme,
         scaffoldBackgroundColor: AppColors.white,
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: AppColors.black,
-        ),
-        appBarTheme: const AppBarTheme(
-          color: AppColors.white,
-          elevation: 0,
-          centerTitle: false,
-          titleSpacing: 20,
-          titleTextStyle: TextStyle(
-            fontSize: 24,
-            color: AppColors.black,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        textTheme: const TextTheme(
-          headline1: TextStyle(
-            fontSize: 24,
-            color: AppColors.black,
-            fontWeight: FontWeight.w500,
-          ),
-          bodyText1: TextStyle(
-            fontSize: 18,
-            color: AppColors.black,
-            fontWeight: FontWeight.w500,
-          ),
-          subtitle1: TextStyle(
-            fontSize: 14,
-            color: AppColors.black,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
+        bottomNavigationBarTheme: bottomNavBarTheme,
+        appBarTheme: appBarTheme,
+        textTheme: textTheme,
       );
 
   static double get defaultBorderRadius => 20;
+
+  static TextTheme get textTheme => const TextTheme(
+        headline1: TextStyle(
+          fontSize: 24,
+          color: AppColors.black,
+          fontWeight: FontWeight.w500,
+        ),
+        bodyText1: TextStyle(
+          fontSize: 18,
+          color: AppColors.black,
+          fontWeight: FontWeight.w500,
+        ),
+        subtitle1: TextStyle(
+          fontSize: 14,
+          color: AppColors.black,
+          fontWeight: FontWeight.w500,
+        ),
+      );
+
+  static AppBarTheme get appBarTheme => const AppBarTheme(
+        color: AppColors.white,
+        elevation: 0,
+        centerTitle: false,
+        titleSpacing: 20,
+        titleTextStyle: TextStyle(
+          fontSize: 24,
+          color: AppColors.black,
+          fontWeight: FontWeight.w500,
+        ),
+      );
+
+  static BottomNavigationBarThemeData get bottomNavBarTheme =>
+      const BottomNavigationBarThemeData(
+        backgroundColor: AppColors.black,
+      );
+
+  static ProgressIndicatorThemeData get progressIndicatorTheme =>
+      const ProgressIndicatorThemeData(
+        color: AppColors.purple,
+      );
 }
